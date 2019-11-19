689927e56d51ec2701a939ff8c7dc8bd6c7a9e19
MidnightLe <leivip3@gmail.com>
2019-11-19 11:11:16 +0800

    测试功能

--- auto_commit_script.py
+++ auto_commit_script.py
@@ -4,6 +4,7 @@ if __name__=='__main__':
     # git commit -m "beizhu "
     # git remote add origin https://github.com/MidnightLe/learnword.git
     # git push origin master
+    defaultgit="https://github.com/MidnightLe/learnword.git"
     os.system("git add .")
     commits=input("请输入备注信息:")
     os.system("git commit -m "+commits)
@@ -14,9 +15,12 @@ if __name__=='__main__':
         os.system("git push origin master")
         
     else:
-        oriname= input("请输入远程库地址:")
+        oriname= input("请输入远程库地址(回车是默认仓库):")
+        if oriname.find("add")==-1:
+            oriname=defaultgit
         os.system("git remote add origin "+oriname)
         os.system("git push -u origin master")
+
 
     a.close()
     print("-"*10+"任务完成"+"-"*10)
